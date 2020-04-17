5d29bc806a395396bcd129df0c8562ca499cbf10
stefanostatuti <stefano.statuti@hotmail.it>
2020-04-17 18:14:41 +0200

    Update CleanRarBGTorrent.sh

--- CleanRarBGTorrent.sh
+++ CleanRarBGTorrent.sh
@@ -1,4 +1,5 @@
 #bin bash
+clear
 echo "******************************************"
 echo "*          RARBG TORRENTS CLEANER        *"
 echo "******************************************"
@@ -6,7 +7,7 @@ echo "* This script removes all .TXT .EXE .JPG *"
 echo "* files from downloaded Torrents from    *"
 echo "* RARBG torrent Site                     *"
 echo "******************************************"
-echo "*     Version 1.1  **Riso64Bit**         *"
+echo "*     Version 1.2  Stefano Statuti       *"
 echo "******************************************"
 echo "Starting...."
 echo ""
@@ -16,31 +17,54 @@ rm LOG_CleanRARBGTorrent.log
 echo "Cleaning OLD log completed !!!"
 echo ""
 echo "Cleaning RARBG.txt and RARBG.com.txt ..." 
-#ls */RARBG.txt >> LOG_CleanRARBGTorrent.log
-#ls */RARBG.com.txt >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+echo " RARBG.txt and RARBG.com.txt FILES DELETED:" >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+ls */RARBG.txt >> LOG_CleanRARBGTorrent.log
+ls */RARBG.com.txt >> LOG_CleanRARBGTorrent.log
 rm */RARBG.txt
 rm */RARBG.com.txt
 echo "...."
 echo "Cleaning txt files completed !!!"
 echo ""
 echo "Cleaning RARBG_DO_NOT_MIRROR.exe ..."
-#ls */RARBG_DO_NOT_MIRROR.exe >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+echo " RARBG_DO_NOT_MIRROR.exe FILES DELETED:    " >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+ls */RARBG_DO_NOT_MIRROR.exe >> LOG_CleanRARBGTorrent.log
 rm */RARBG_DO_NOT_MIRROR.exe
 echo "...."
 echo "Cleaning RARBG_DO_NOT_MIRROR.exe completed !!!"
 echo ""
 echo "Cleaning ALL nfo files ..." 
-#ls */*.nfo >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+echo " .NFO FILES DELETED:                       " >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+ls */*.nfo >> LOG_CleanRARBGTorrent.log
 rm */*.nfo
 echo "...."
 echo "Cleaning nfo files completed !!!"
 echo ""
 echo "Cleaning ALL jpg files ..."
-#ls */*.jpg >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+echo " .JPG FILES DELETED:                       " >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+ls */*.jpg >> LOG_CleanRARBGTorrent.log
 rm */*.jpg
 echo "...."
 echo "Cleaning jpg files completed !!!"
-#echo less LOG_CleanRARBGTorrent.log
+echo ""
+echo ""
+echo "Cleaning ALL Sample files/folders ..."
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+echo " Sample Files and folders can be DELETED:  " >> LOG_CleanRARBGTorrent.log
+echo "-------------------------------------------" >> LOG_CleanRARBGTorrent.log
+ls -di */Sample >> LOG_CleanRARBGTorrent.log
+ls -i */Sample  >> LOG_CleanRARBGTorrent.log
+echo "...."
+echo "Cleaning Sample files/folders completed !!!"
+echo ""
+echo "Script's log is here: LOG_CleanRARBGTorrent.log"
 echo ""
 echo "Well Done, All Completed!!!"
 echo "******************************************"
